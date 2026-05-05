33b7e8d465a9cc170cff5fa0b5d1d795de3f53ed
Yuris20 <sangiuliano.yuri@gmail.com>
2026-05-05 18:50:08 +0200

    fix sicurezza

--- CeCorrente/src/inc/_global/views/footer_start.php
+++ CeCorrente/src/inc/_global/views/footer_start.php
@@ -6,15 +6,7 @@
  */
 ?>
 
-<!--
-    assets/js/core/jquery.min.js
-    assets/js/core/bootstrap.bundle.min.js
-    assets/js/core/simplebar.min.js
-    assets/js/core/jquery-scrollLock.min.js
-    assets/js/core/jquery.appear.min.js
-    assets/js/core/jquery.countTo.min.js
-    assets/js/core/js.cookie.min.js
--->
+
 <script src="<?php echo $cb->assets_folder; ?>/js/codebase.core.min.js"></script>
 
 <!--
